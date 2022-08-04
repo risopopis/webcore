e11b0a9eda43a8638259d4d075492549e82eac92
Jane <102874239+risopopis@users.noreply.github.com>
2022-08-04 18:08:41 +0300

    fixed slider

--- 1__5/1_5/script.js
+++ 1__5/1_5/script.js
@@ -1,29 +1,25 @@
 
-let showMore=document.querySelector('.slider__items_hidden');
-let buttonMore=document.querySelector('.button_show-more');
-let closeButton=document.querySelector('.button_hide');
+let showMore = document.querySelector('.slider__items_hidden');
+let buttonMore = document.querySelector('.button_show-more');
+let closeButton = document.querySelector('.button_hide');
 
 buttonMore.addEventListener('click', function () {
   showMore.classList.remove('hidden');
   buttonMore.classList.add('hidden');
   closeButton.classList.remove('hidden');
-  });
+});
 closeButton.addEventListener('click', function () {
   showMore.classList.add('hidden');
   closeButton.classList.add('hidden');
   buttonMore.classList.remove('hidden');
-  })
+})
 
   const swiper = new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true
     },
-    autoHeight: true,
-    spaceBetween: 16,
-    slidesPerView: 1.2,
-    centeredSlides: false,
-    breakpoints: {
+       breakpoints: {
         768: {
             enabled: false,
             onAny() {
@@ -33,3 +29,6 @@ closeButton.addEventListener('click', function () {
     },
     observer: true,
 })
+
+
+
